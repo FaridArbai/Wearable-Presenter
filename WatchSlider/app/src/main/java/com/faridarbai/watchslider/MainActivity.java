3bcd024cb3e49bd872c9962f8eb466e19eca29b3
Farid Arbai <35288820+FaridArbai@users.noreply.github.com>
2018-06-14 23:31:27 +0200

    Enhanced GUI for rounded watches

--- WatchSlider/app/src/main/java/com/faridarbai/watchslider/MainActivity.java
+++ WatchSlider/app/src/main/java/com/faridarbai/watchslider/MainActivity.java
@@ -1,52 +1,84 @@
-package com.faridarbai.watchslider;
+package com.faridarbai.watchsliderbeta;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
+import android.support.wearable.activity.WearableActivity;
 import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+import java.io.PrintWriter;
+import java.net.Socket;
+
+public class MainActivity extends WearableActivity {
+    static public String SERVER_IP = "192.168.0.157";
+    static public int SERVER_PORT = 1010;
+
+    static public Button next_button;
+    static public Button back_button;
+
+    static public String BACK_CODE = "BACK\n";
+    static public String NEXT_CODE = "NEXT\n";
+
+    static public Socket socket;
+    static public PrintWriter writer;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        setAmbientEnabled();
+
+        ConnectTask ctask = new ConnectTask("CONNECT");
+        ctask.execute();
+
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
+    class ConnectTask extends AsyncTask<Void, Void, Void> {
+        String type;
+        String message;
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        public ConnectTask(String type) {
+            super();
+            this.type = type;
         }
 
-        return super.onOptionsItemSelected(item);
+        public ConnectTask(String type, String message) {
+            super();
+            this.type = type;
+            this.message = message;
+        }
+
+        @Override
+        protected Void doInBackground(Void... voids) {
+            try {
+                if (type.equals("CONNECT")) {
+                    socket = new Socket(SERVER_IP, SERVER_PORT);
+                    writer = new PrintWriter(socket.getOutputStream());
+                } else {
+                    writer.write(this.message);
+                    writer.flush();
+                }
+
+            } catch (Exception ex) {
+                ex.printStackTrace();
+            }
+
+            return null;
+        }
     }
+
+    public void onNextClicked(View v){
+        ConnectTask ctask = new ConnectTask("MESSAGE", NEXT_CODE);
+        ctask.execute();
+    }
+
+    public void onBackClicked(View v){
+        ConnectTask ctask = new ConnectTask("MESSAGE", BACK_CODE);
+        ctask.execute();
+    }
+
+
+
 }
